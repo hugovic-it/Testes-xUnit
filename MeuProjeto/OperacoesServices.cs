8c5fdd7fe879f759fad8dce9802b3301c99ca19a
Hugo Victor <hugovic.it@gmail.com>
2023-09-21 10:44:28 -0300

    Console base

---
+++ MeuProjeto/OperacoesServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuProjeto
+{
+    internal static class OperacoesServices
+    {
+        public static bool UsuarioMaiorDeIdade(Usuario user) 
+        { 
+            if(user.Idade >= 18){
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ MeuProjeto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MeuProjeto;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olár");
+
+        Usuario user1 = new Usuario() { Nome = "Joao", Idade=16,Peso=60};
+        Usuario user2 = new Usuario() { Nome = "Jose", Idade= 21, Peso = 75 };
+
+        bool resultado1 = OperacoesServices.UsuarioMaiorDeIdade(user1);
+        Console.WriteLine($"O usuário {user1.Nome} é maior de idade? {resultado1}" );
+        bool resultado2 = OperacoesServices.UsuarioMaiorDeIdade(user2);
+        Console.WriteLine($"O usuário {user2.Nome} é maior de idade? {resultado2}");
+    }
+}
